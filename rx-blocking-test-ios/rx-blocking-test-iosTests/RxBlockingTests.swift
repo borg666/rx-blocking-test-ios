8173782ef0f99697255984e272b6445025fd57dd
keran.marinov <keran.fm@gmail.com>
2017-11-06 22:30:51 +0000

    observerable collector

--- rx-blocking-test-ios/rx-blocking-test-iosTests/RxBlockingTests.swift
+++ rx-blocking-test-ios/rx-blocking-test-iosTests/RxBlockingTests.swift
@@ -9,23 +9,34 @@ import RxTest
 class RxBlockingTests: XCTestCase {
 
     var isVisible: Variable<Bool> = Variable<Bool>(false)
+    var isHidden: Observable<Bool> = Observable<Bool>.create { observer in
+        observer.onNext(false)
+        observer.onNext(true)
+        observer.onCompleted()
+        return Disposables.create()
+    }
     var onTapped: PublishSubject<Void> = PublishSubject<Void>()
     var disposeBag: DisposeBag = DisposeBag()
 
-    func testBoolCollector() {
-        let collector = BoolCollector().collect(from: &isVisible)
+    func testVariableBoolCollector() {
+        let collector = VariableBoolCollector().collect(from: isVisible)
         isVisible.value = false
         isVisible.value = false
         isVisible.value = true
-        XCTAssertEqual(collector.toArray, [true, true])
+        XCTAssertEqual(collector.toArray, [false, false, false, true])
+    }
+
+    func testObservableBoolCollector() {
+        let collector = ObservableBoolCollector().collect(from: isHidden)
+        XCTAssertEqual(collector.toArray, [false, true])
     }
 
     func testVoidCollector() {
-        let collector = VoidCollector().collect(from: &onTapped)
+        let collector = VoidCollector().collect(from: onTapped)
         onTapped.onNext(())
         onTapped.onNext(())
         onTapped.onNext(())
-        XCTAssertEqual(collector.toArray.count, 4)
+        XCTAssertEqual(collector.toArray.count, 3)
     }
 }
 
@@ -33,7 +44,7 @@ class VoidCollector {
     var disposeBag: DisposeBag = DisposeBag()
     var toArray: [Void] = [Void]()
 
-    func collect( from variable: inout PublishSubject<Void>) -> Self {
+    func collect( from variable: PublishSubject<Void>) -> VoidCollector {
         variable.asObservable()
             .subscribe(onNext: { () in
                 self.toArray.append(())
@@ -42,12 +53,25 @@ class VoidCollector {
     }
 }
 
-class BoolCollector {
-     var disposeBag: DisposeBag = DisposeBag()
-     var toArray: [Bool] = [Bool]()
+class VariableBoolCollector {
+    var disposeBag: DisposeBag = DisposeBag()
+    var toArray: [Bool] = [Bool]()
 
-     func collect( from variable: inout Variable<Bool>) -> Self {
+    func collect( from variable: Variable<Bool>) -> VariableBoolCollector {
         variable.asObservable()
+            .subscribe(onNext: { (isVisible) in
+                self.toArray.append(isVisible)
+            }).addDisposableTo(disposeBag)
+        return self
+    }
+}
+
+class ObservableBoolCollector {
+    var disposeBag: DisposeBag = DisposeBag()
+    var toArray: [Bool] = [Bool]()
+
+    func collect( from observable: Observable<Bool>) -> ObservableBoolCollector {
+        observable.asObservable()
             .subscribe(onNext: { (isVisible) in
                 self.toArray.append(isVisible)
             }).addDisposableTo(disposeBag)
